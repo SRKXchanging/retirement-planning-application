cd7b43642e6b652a335cb9c2a1e37a0b068d0ac6
lhy-hoyin <88828097+lhy-hoyin@users.noreply.github.com>
2024-05-25 19:18:03 +0800

    Implement getters for retirement and insurance data

---
+++ data-retriever.mjs
@@ -0,0 +1,47 @@
+import retirementProducts from './Retirement.json' assert { type: 'json' };
+import insuranceProducts from './insurance.json' assert { type: 'json' };
+
+/** Retirement Products **/
+
+function getRetirementProducts() {
+    return retirementProducts['products']
+}
+
+function getRetirementProductById(id) {
+    var result = getRetirementProducts().find(elem => elem['ID'] == id);
+    return result == undefined ? null : result;
+}
+
+function getProductsByBrand(brand) {
+    var result = getRetirementProducts().filter(elem => elem['Brand'] == brand);
+    return result == undefined ? null : result;
+}
+
+/** Insurance Products **/
+
+function getInsuranceProducts() {
+    return insuranceProducts['products']
+}
+
+function getInsuranceProductById(id) {
+    var result = getInsuranceProducts().find(elem => elem['ID'] == id);
+    return result == undefined ? null : result;
+}
+
+function getInsuranceProductsByBrand(brand) {
+    var result = getInsuranceProducts().filter(elem => elem['Brand'] == brand);
+    return result == undefined ? null : result;
+}
+
+function getInsuranceProductsByType(filter) {
+    var result = getInsuranceProducts().filter(elem => elem['Filter'] == brand);
+    return result == undefined ? null : result;
+}
+
+/** Sample Code **/
+
+// ID can be provided as a int or string
+console.log(getRetirementProductById(1))
+console.log(getRetirementProductById('1'))
+
+console.log(getProductsByBrand('DBS')[0])
